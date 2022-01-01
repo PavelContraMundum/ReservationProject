4f340ce9ca5fe59eebfda44831dc5ddd72ac37d5
PavelContraMundum <pavel.sticka@gmail.com>
2022-01-01 11:03:03 +0100

    nahrazeni array pomoci slice

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 	// lze zapsat i takto: conferenceName := "Go Conference" Takto nelze deklarovat konstanty, nelze také specificky deklarovat datový typ
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50 // uint nemůže být záporné číslo
-	var bookings [50]string
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
@@ -30,12 +30,13 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
-	bookings[0] = firstName + " " + lastName
 
-	fmt.Printf("The whole array: %v\n", bookings)
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("The whole slice: %v\n", bookings)
 	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Array type: %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
+	fmt.Printf("Slice type: %T\n", bookings)
+	fmt.Printf("Slice length: %v\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaing for %v\n", remainingTickets, conferenceName)
